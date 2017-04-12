ab9534021f5fc7f4538a10ff9f2f432db2f9cc25
ahchew120 <lpc_120@hotmail.com>
2017-04-12 11:57:27 +0800

    commit

--- mailer.php
+++ mailer.php
@@ -5,7 +5,7 @@
         $name = strip_tags(trim($_POST["name"]));
 				$name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["message"]);
+        //$message = trim($_POST["message"]);
 
         // Check that data was sent to the mailer.
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
